d2e3f3ba59f53725fba83ce109c2e1f3776a155f
Harshavardhan <187y1a0575@gmail.com>
2020-09-28 15:27:03 +0530

    Completed counter website, created using svelte

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+	let count=0;
+	const increment=()=>{
+		count+=1;
+	}
+</script>
+
+<div class='main'>
+	<h1>Counter</h1>
+	<h2>Created by Harshavardhan using svelte</h2>
+	<button on:click={increment}>Counter button clicked {count} {count===1? 'time': 'times'}</button>
+</div>
+<style>
+	h1{
+		text-align: center;
+		font-size: 3.5rem;
+	}
+	h2{
+		text-align: center;
+	}
+	button{
+		display: block;
+		margin-right: auto;
+		margin-left: auto;
+		cursor:pointer;
+	}
+</style>
